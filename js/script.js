e09b5693bcb985ee135d870f203210ad28678fa8
Zheka-ded <11vitamin11@rambler.ru>
2019-09-29 09:53:50 +0300

    animation for letters is ready

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
+// (function strToArr (){
+//     let arrText = document.querySelectorAll(`.title__text`);
 
-(function strToArr (){
+//     for(i = 0; i < arrText.length; i++){
+//         let str = arrText[i].innerText;
+//         let chars = str.split('');
+//         let chars_in_span = chars.map(char => `<span class="span__title" data-text="${char}">${char}</span>`);
+//         let new_html = chars_in_span.join('');
+//         arrText[i].innerHTML = new_html;
+
+//     }
+
+// }());
+
+
+// Функция перевода слов в отдельные буквы в span
+(function (){
+
     let arrText = document.querySelectorAll(`.title__text`);
 
     for(i = 0; i < arrText.length; i++){
-        let str = arrText[i].innerText;
-        let chars = str.split('');
-        let chars_in_span = chars.map(char => `<span class="span__title" data-text="${char}">${char}</span>`);
-        let new_html = chars_in_span.join('');
-        arrText[i].innerHTML = new_html;
-
+        let inSpan = arrText[i].innerText.split('').map(function(symb){
+            return `<span class="span__title" data-text="${symb}">${symb}</span>`
+        });
+        arrText[i].innerHTML = inSpan.join('');
     }
 
 }());
 
 
+// Функция анимации для букв
+document.querySelectorAll(`.span__title`).forEach(function(element){
+    element.onmouseenter = spanAddStyle;
+});
+
+function spanAddStyle() {
+    this.classList.add(`span__title-active`);
+    setTimeout(function () {
+        document.querySelectorAll(`.span__title`).forEach(function(element){
+            element.classList.remove(`span__title-active`);
+        })
+    }, 800);
+};
 
 
 
-// document.querySelectorAll(`.span__title`).forEach(function(element){
-//     element.onmouseenter = spanAddStyle;
-// });
 
 // function spanAddStyle() {
 //     this.style.color = `red`;
